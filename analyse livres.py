fae8feb1a663884dc9df20d24baefbd973998b7c
Louis de Monterno <louis.penetdemonterno@gmail.com>
2018-06-15 15:58:44 +0200

    fréquence des mots -> fichier csv
    - parcours automatique du dossier de livres
    - en cours : recherche de mots pertinents avec tf-idf

--- analyse livres.py
+++ analyse livres.py
@@ -5,23 +5,64 @@
 Ceci est un script temporaire.
 """
 
+from os import listdir
+from os.path import isfile, join
+import csv
+
 mots = ["amour", "guerre", "homme", "femme", "courage"\
         , "argent", "dieu", "arme", "oeuvre"]
 
-fichier = ["voyage au bout de la nuit", "la peste", "l'étranger"]
+mots.sort()
 
-for livre in fichier:
-    
-    features = [0] * len(mots)
-    
-    with open("livres/"+livre, "r", encoding="utf-8") as fichier:
-        
-        ensemble_mots = fichier.read().split(' ')
-        
-        for mot in ensemble_mots:
+def insertion(liste, el):
+    a = -1
+    b = len(liste)
+    while a + 1 < b:
+        temp = liste[(a+b)//2][0]
+        if temp < el:
+            a = (a+b)//2
+        elif temp > el:
+            b = (a+b)//2
+        else:
+            liste[(a+b)//2][3] += 1
+            return
+    liste.insert(b,[el,0,0,1])
+
+def mots_pertinents(nombre):
+     with open("données_livres.csv","w",encoding="utf-8") as fichier:
+         idf = []
+         for livre in listdir("./livres/"):
+            if not isfile("./livres/" + livre):
+                continue
             
-            if mot in mots:
-                
-                features[mots.index(mot)] += 1
-    
-    print(livre + " -->    " + str(features))
+            with open("livres/"+livre, "r", encoding="utf-8") as fichier:
+                ensemble_mots = fichier.read().split(' ')
+                insertion(idf,mot.lower())
+            
+            for mots in idf if mots[3] != 0:
+                mots[1] += mots[3]
+                mots[2] += 1
+                mots[3] = 0
+    for mots in idf:
+        mots[1] = mots
+    sorted(idf, 0)
+         
+
+def livre2csv():
+    with open("données_livres.csv","w",encoding="utf-8") as fichier:
+        curseur = csv.writer(fichier, delimiter=",", quotechar="\"")
+        curseur.writerow(["livres"] + mots)
+        for livre in listdir("./livres/"):
+            
+            if not isfile("./livres/" + livre):
+                continue
+            features = [0] * len(mots)
+            
+            with open("livres/"+livre, "r", encoding="utf-8") as fichier:
+                ensemble_mots = fichier.read().split(' ')
+                for mot in ensemble_mots:
+                    mot.lower()
+                    if mot in mots:    
+                        features[mots.index(mot)] += 1
+            
+            curseur.writerow([livre] + features)

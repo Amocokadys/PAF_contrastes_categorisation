c9edebcd64e7bcac71390b2ceb46ec81138cf1d3
Benoît Malézieux <benoit.malezieux@free.fr>
2018-06-15 17:04:03 +0200

    modification k-means

--- kmeans.py
+++ kmeans.py
@@ -2,6 +2,11 @@
 
 import pandas as pd
 from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+
 
 
 class PafKmeans(object):
@@ -12,8 +17,18 @@ class PafKmeans(object):
     def kmeans(self):
         self.model = KMeans(n_clusters = self.number)
         self.model.fit(self.dataframe)
-    def findN(self):
-        return None
+    def sseTab(self):
+        sse = []
+        for clusters_number in range(1, 10):
+            sse.append(0)
+            self.number = clusters_number
+            self.kmeans()
+            for cluster in range(clusters_number):
+                mean = self.model.cluster_centers_[cluster]
+                for k, row in self.dataframe[self.dataframe["category"] == cluster].iterrows():
+                    
+                    sse[clusters_number-1] += (row - mean)**2            
+        return sse
     def newDataFrame(self):
         self.dataframe['category'] = pd.Series(self.model.labels_, index = self.dataframe.index)
     def result(self):
@@ -31,5 +46,9 @@ pafkmeans = PafKmeans(test)
 
 centers, data = pafkmeans.result()
 
-print(centers)
-print(data)
+sse = pafkmeans.sseTab()
+plt.plot(np.arange(1, 10), sse, 'ro')
+plt.show()
+
+print(sse)
+

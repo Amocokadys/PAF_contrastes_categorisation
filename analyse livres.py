0e641dfa562db185020a04f36eab4fb67b519153
Louis de Monterno <louis.penetdemonterno@gmail.com>
2018-06-18 16:25:53 +0200

    création du jeu de données fonctionnelle
    ajout de commentaires

--- analyse livres.py
+++ analyse livres.py
@@ -11,12 +11,26 @@ import math
 import pandas
 
 def remplir_dico(dico):
+	
+	""" charge en mémoire le dictionnaire français,
+	et en retire les mots listés dans le fichier prévu à cet effet """
+	
+	exclure = set()
+	with open("jeux de donne/mots à exclure", "r", encoding="utf-8") as fichier:
+		cursor = csv.reader(fichier, delimiter=";")
+		for el in cursor:
+			if len(el) > 0:
+				exclure.add(el[0])
 	with open("jeux de donne/dictionnaire", "r", encoding="utf-8") as fichier:
 		cursor = csv.reader(fichier, delimiter=";")
 		for el in cursor:
-			dico.add(el[0])
+			if not el[0] in exclure:
+				dico.add(el[0])
 
 class Iter_bouquin:
+	
+	""" classe servant à parcourir l'ensemble des mots d'un livre
+	en excluant les mots hors du dictionnaire et les mots d'une lettre """
 	
 	def __init__(self, fichier):
 		fich = open("livres/" + fichier, "r")
@@ -32,9 +46,11 @@ class Iter_bouquin:
 				break
 	
 	def __iter__(self):
+		self.nombr_de_mots = 0
 		return self
 	
-	def __next__(self):
+	def __next__(self): # cherche le mot suivant
+		self.nombr_de_mots += 1
 		compteur = 0
 		for i in range(self.i_début, len(self.bouquin)):
 			if self.bouquin[i].lower() in "abcdefghijklmnopqrstuvwxyzéèàçùêâ-":
@@ -53,6 +69,8 @@ class Iter_bouquin:
 
 class Tf_idf:
 	
+	""" classe cherchant les mots pertinents avec TF-IDF """
+	
 	nombre_bouquins = 0
 	
 	def __init__(self):
@@ -63,6 +81,10 @@ class Tf_idf:
 	
 
 	def insertion(self, el):
+		
+		""" recherche dichotomique avec insertion,
+		conservant l'ordre lexicographique """
+		
 		a = -1
 		b = len(self.idf)
 		while a + 1 < b:
@@ -109,20 +131,27 @@ def mots_pertinents(nombre):
 			meilleurs[meilleurs.index(min(meilleurs, key=lambda x:x[1]))] = (mots[0],toto)
 			minimum = min(meilleurs, key = lambda x:x[1])[1]
 		
-	return meilleurs.sort(key=lambda x:-x[1])
+	meilleurs.sort(key=lambda x:-x[1])
+	for el in meilleurs:
+		print(el[0])
 
 def livre2csv():
+	pertients = []
+	with open("jeux de donne/mots pertinents", "r", encoding="utf-8") as fichier:
+		cursor = csv.reader(fichier, delimiter=";")
+		for el in cursor:
+			pertients.append(el[0])
 	with open("jeux de donne/bibliothq.csv","w",encoding="utf-8") as fichier:
 		curseur = csv.writer(fichier, delimiter=",", quotechar="\"")
-		curseur.writerow(["livres"] + mots)
+		curseur.writerow(["livres"] + pertients)
 		for livre in listdir("./livres/"):
-			features = [0] * len(mots)
+			print("construction de " + livre + " ...")
+			features = [0] * len(pertients)
 			
-			with open("livres/"+livre, "r", encoding="utf-8") as fichier:
-				ensemble_mots = fichier.read().split(' ')
-				for mot in ensemble_mots:
-					mot.lower()
-					if mot in mots:	
-						features[mots.index(mot)] += 1
+			bouquin = Iter_bouquin(livre)
+			for mot in bouquin:
+				if mot in pertients:
+					features[pertients.index(mot)] += 1
+			features = [features[i]/bouquin.nombr_de_mots for i in range(len(pertients))]
 			
 			curseur.writerow([livre] + features)

5aff35cee347f36688107d4618f4a404e53e0eee
Antoine Bellami <antoine@localhost.localdomain>
2018-06-18 11:12:35 +0200

    Notre fonction de nettoyage de k-means renvoie désormais une liste de dataframes

--- src/algo.py
+++ src/algo.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3.6
 
-import sys
 import pandas as pd
 import numpy as np
-import sklearn.metrics as sm
-import matplotlib.pyplot as plt
 import math as m
 
+# returns a list of dataframes (one for each core cluster + one for the remaining points) and the centers
 def clean_kmeans(data, centers):
     nb_clusters = len(centers)
     centers=np.array(centers)
@@ -17,25 +15,42 @@ def clean_kmeans(data, centers):
         clusters.append(data.loc[data['category']==i]) # for each cluster, selects the points in the cluster
 
      # removes the cluster feature from the dataset
-     for c in clusters:
-         del(c["category"])
+    for c in clusters:
+        del(c["category"])
 
     # clean clusters by selecting the 20% points of the cluster nearest to the center
-    clean_clusters = []
+    # we build as many dataframes as the number of clusters, and a dataframe which contains remaining data
+    dataframeList = []
+    remainingData = []
     for i in range(nb_clusters):
         srt_data = sorted(clusters[i].values, key = lambda x:sum((x-centers[i])**2))
-        clean_clusters.append(srt_data[:m.ceil(0.2*len(clusters[i]))+1])
+        coreCluster = srt_data[:m.ceil(0.2*len(clusters[i]))+1]
+        
+        remainingData.append(srt_data[m.ceil(0.2*len(clusters[i]))+1:])
+        
+        coreDataframe = pd.DataFrame(coreCluster, columns=["x", "y"])
 
-    clean_clusters = np.array(clean_clusters)
+        dataframeList.append(coreDataframe)
+        
+    remainingData = np.array(remainingData)
+    
+    #tmpData is useful to list the points' coordinates in a single array
+    tmpData=[]
+    for i in range(len(remainingData)):
+        for j in range(len(remainingData[i])):
+            tmpData.append(remainingData[i][j])
+            
+    remainingData = pd.DataFrame(np.array(tmpData), columns=["x", "y"])
+    dataframeList.append(remainingData)
 
-    return clean_clusters
+    return dataframeList, centers
 
 
 if __name__=="__main__":
     d = pd.DataFrame(np.array([[-5, 5, 0], [-4, 4, 0], [-7, 4, 0], [-4, 7, 0], [-7, 7, 0],\
                                [5, -5, 1], [4, -4, 1], [7, -4, 1], [4, -7, 1], [7, -7, 1]]),\
                      columns=["x", "y", "category"])
-    print(d)
+    
     centers = [[-5, 5], [5, -5]]
-    dp = clean_kmeans(d, centers)
-    print(dp)
+    dataframesList = clean_kmeans(d, centers)
+    print(dataframesList)

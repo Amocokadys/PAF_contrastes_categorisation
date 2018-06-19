a96f86ade5b33ed24fea536cfebb4e4dbe5d3492
Louis de Monterno <louis.penetdemonterno@gmail.com>
2018-06-19 11:33:08 +0200

    création d'une classe manipulant les arbres

---
+++ src/processing/arborescence.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 19 10:57:46 2018
+
+@author: louis
+"""
+
+import cluster
+
+class Arbre:
+	
+	def __init__(self, enfants, pater=None, label=None):
+		self.enfants = enfants
+		self.label = label
+		self.pater = pater
+		self.groupe = Cluster(enfants, None, label)
+		
+	
+	def distance(self, )
+	
+	def ajout(self, livre):
+		
+
--- src/processing/cluster.py
+++ src/processing/cluster.py
@@ -31,14 +31,19 @@ class Cluster:
     
     
     def __init__(self,points,centre,numero):
-        if (len(points) == 2) :
-            pointMoyen = [(points[i][0] + points[i][1] + 1.0001)/2 for i in points.columns]
-            new_data = pd.DataFrame([pointMoyen], columns = points.columns, index = pd.RangeIndex(start=2, stop=3, step=1))
-            points = points.append(new_data)
-        self.centre=centre
-        self.numero=numero
-        self.points=points
-        self.matriceCov=self.matriceCovariance(points)
+		if (len(points) == 2) :
+			pointMoyen = [(points[i][0] + points[i][1] + 1.0001)/2 for i in points.columns]
+			new_data = pd.DataFrame([pointMoyen], columns = points.columns, index = pd.RangeIndex(start=2, stop=3, step=1))
+			points = points.append(new_data)
+		if centre == None:
+			centre = [0]*len(points[0])
+			for i in range(len(points)):
+				centre = [centre[j]+points[i][j] for j in range(len(points[0]))]
+			centre = [centre[i]/len(points) for i in range(len(points))]
+		self.centre=centre
+		self.numero=numero
+		self.points=points
+		self.matriceCov=self.matriceCovariance(points)
         
     """ this function calculates the distance from a point to a cluster in terms of
         number of standard deviations

75448c30d6054aaf33b53db3a395b89b784a8878
Louis de Monterno <louis.penetdemonterno@gmail.com>
2018-06-19 13:42:24 +0200

    ajout d'une méthode d'actualisation des matrices de covariances dans l'arbre avec recherche récursive de noeuds

--- src/processing/arborescence.py
+++ src/processing/arborescence.py
@@ -7,8 +7,20 @@ Created on Tue Jun 19 10:57:46 2018
 """
 
 import cluster
+import math
+import numpy as np
+import pandas as pd
 
-class Arbre:
+class Arbre(Cluster):
+	
+	def __init__(self, points):
+		
+	
+	def distance(self, point):
+		point_centre = np.array([point]) - np.array([centre])
+		return math.sqrt(np.dot(	np.dot(point_centre,np.inv(self.matriceCov)),\
+								np.transpose(point_centre)))
+
 	
 	def __init__(self, enfants, pater=None, label=None):
 		self.enfants = enfants
@@ -16,9 +28,24 @@ class Arbre:
 		self.pater = pater
 		self.groupe = Cluster(enfants, None, label)
 		
-	
-	def distance(self, )
-	
+		
 	def ajout(self, livre):
+		1+1
+	
+	def actualise_matrice(self):
+		liste_points = self._private_liste_points()
+		self.centre = liste_points.mean()
+		self.matriceCov = self.matriceCovariance(liste_points)
+		random = np.random.random(len(enfants[0]),len(enfants[0]))
+		self.matriceCov += pd.DataFrame(random * 10e-6)
+	
+	def _private_liste_points(self):
+		points = []
+		for el in self.enfants:
+			if type(el) == tuple:
+				points += el
+			else:
+				points += el.actualise()
+		return points
 		
 

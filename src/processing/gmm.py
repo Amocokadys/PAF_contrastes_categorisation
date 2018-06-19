534b1688fc2995a2cdf39bf8ed3c8d4ce3658442
Benoît Malézieux <benoit.malezieux@free.fr>
2018-06-19 19:55:01 +0200

    gmm avec categorisation et contraste

--- src/processing/gmm.py
+++ src/processing/gmm.py
@@ -10,170 +10,77 @@ Date …………………………………………………… : 19/06/2018
 Description du fichier …… : Cartouche
 """
 
+
 import numpy as np
 import pandas as pd
 
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 
-from sklearn.cluster import KMeans
 from sklearn.mixture import GaussianMixture
 
-#from sklearn import metrics
 
-class PafKmeans:
-    """ class with attributs a KMeans objects 
+
+class PafGMM:
+    """ class with attributs a GaussianMixture objects 
         the dataframe on which we use kmean
         the number of clusters we want
     """
     
-    def __init__(self, dataframe):
+    def __init__(self, dataframe, number):
         self.dataframe = dataframe
         self.model = None
-        self.number = 4
+        self.number = number
         
-    def kmeans(self, number):
-        """ execute kmeans for n_clusters=numbe r"""
-        self.model = KMeans(n_clusters = number)
+    def gmm(self, number):
+        """ execute gmm for n_clusters=number"""
+        self.model = GaussianMixture(n_components = number)
         self.model.fit(self.dataframe)
-        
-    def sseTab(self):
-        """ return the distorsion in all clusters, distorsion = mean distance with cluster's center """
-        sse = []
-        for clusters_number in range(1, 30):
-            sse.append(0)
-            self.kmeans(clusters_number)
-            sse[clusters_number-1] += self.model.inertia_           
-        return sse
-        
-    def findElbow(self):
-        """ method to find k through the elbow method with slopes"""
-        sse = self.sseTab()
-        pentes = []
-        for i in range(1, len(sse)):
-            pentes.append(sse[i] - sse[i-1])
-            
-        k=1
-        ang=180
-        while(k<len(pentes)-1 and (abs(180-ang*180/np.pi)>110)):
-            ang=abs(np.arctan(pentes[0])-np.arctan(pentes[k]))
-            k+=1
-            
-        self.number=k
         
     def newDataFrame(self):
         """ adds the column category in the dataframe with the labels of the clusters """
-        self.dataframe['category'] = pd.Series(self.model.labels_, index = self.dataframe.index)
+        self.dataframe['category'] = pd.Series(self.model.predict(self.dataframe), index = self.dataframe.index)
         
     def result(self):
         """ return the coordinates of cluster's centers and the dataframe"""
-        self.findElbow()
-        self.kmeans(self.number)
+        #self.findElbow()
+        self.gmm(self.number)
         self.newDataFrame()
-        return self.model.cluster_centers_, self.dataframe
-    
-
-def test_fruits():
-    test = pd.read_csv("../../fruitsModified.csv")
-    del test["Unnamed: 0"]
-    pafkmeans=PafKmeans(test)
-    #centers,data = pafkmeans.result()
-    return pafkmeans
-
-def test_livres():
-    test=pd.read_csv("../../bibliothq.csv")
-    test.index=test["livres"]
-    del test["livres"]
-    pafkmeans = PafKmeans(test)
-    #centers, data = pafkmeans.result()
-    return pafkmeans
-
-def graphic_elbow(pafkmeans,title):
-    """ 
-        Trace the elbow curve
-        arg : Pafkmeans object    
-    """
-    centers, data = pafkmeans.result()
-    sse = pafkmeans.sseTab()
-    plt.plot(np.arange(1, 30), sse, 'ro')
-    plt.xlabel("k")
-    plt.ylabel("distorsion") #distorsion = Somme variance (distance avec centre cluster)
-    plt.title(title)
-    plt.show()
-
-def graphic_clusters_fruits(pafkmeans):
-    #Visualisation des clusters formés par K-Means
-    centers,data=pafkmeans.result()
-    plt.scatter(data.longueur,data.fibres,c=pafkmeans.model.labels_.astype(np.float),edgecolor='k')
-    plt.title('Classification K-means ')
-    plt.xlabel("longueur")
-    plt.ylabel("fibres")
-    plt.show()
-    
-    plt.scatter(data.r,data.longueur,c=pafkmeans.model.labels_.astype(np.float),edgecolor='k')
-    plt.xlabel("rouge")
-    plt.ylabel("longueur")
-    plt.show()
-    
-    #cluster en 3D
-    fig=plt.figure(1,figsize=(4,3))
-    ax=Axes3D(fig,rect=[0,0,0.95,1],elev=48,azim=134)
-    ax.scatter(data.r, data.v, data.b,c=pafkmeans.model.labels_.astype(np.float), edgecolor='k')
-
-    ax.w_xaxis.set_ticklabels([])
-    ax.w_yaxis.set_ticklabels([])
-    ax.w_zaxis.set_ticklabels([])
-
-    plt.title("Classification k-mean 3D")
-
-    ax.dist = 12
-    plt.show()
+        return self.dataframe
     
     
-def datasList(data):
-    """ create list of datas """
-    data = data.sort_values(by='category')
-    ordo = [[] for k in range(len(data.columns)-1)] #datas
-    absc = [] #categories
-    for row in data.iterrows():
-        absc.append(row[1][len(data.columns)-1])
-        for k in range(len(data.columns)-1):
-            ordo[k].append(row[1][k])
-    return ordo,absc
+class Contraste:
+    def __init__(self, categorisation, means, number):
+        self.categorisation = categorisation
+        shape = np.shape(means)
+        self.means = np.append(means, np.zeros((shape[0], 1)), axis=1)
+        self.contraste = pd.DataFrame([], columns = self.categorisation.columns)
+        self.pafgmm = None
+        self.number = number
+    def createContraste(self):
+        for k, row in self.categorisation.iterrows():
+            new = row - self.means[int(row["category"])]
+            self.contraste = self.contraste.append(new)
+        del self.contraste["category"]
+    def calcul(self):
+        self.createContraste()
+        self.pafgmm = PafGMM(self.contraste, self.number)
+        return self.pafgmm.result()
     
-def afficherDatasCategory(data):
-    """ print all datas/categories """
-    ordo,absc=datasList(data)
-    for k in range(len(data.columns)-1):
-        plt.plot(absc,ordo[k],'or')
-        plt.xlabel("categories")
-        plt.ylabel(data.columns[k])
-        plt.title(data.columns[k] + "/category")
-        plt.show()
+    
+        
+    
+data = pd.read_csv("fruitsModified.csv")
+data.index = data["Unnamed: 0"]
+del data["Unnamed: 0"]
 
 
-def main():
-    pafkmean_livres=test_livres()
-    pafkmean_fruits=test_fruits()
-    graphic_elbow(pafkmean_livres,"Elbow of books")
-    graphic_elbow(pafkmean_fruits,"Elbow of fruits")
-    graphic_clusters_fruits(pafkmean_fruits)
-    
-    afficherDatasCategory(pafkmean_livres.dataframe)
-    afficherDatasCategory(pafkmean_fruits.dataframe)
-    
-    print("k_fruits = ",pafkmean_fruits.number)
-    print("k_books = ",pafkmean_livres.number)
-    
-if __name__ == "__main__":
-    main()
-    """
-    gmm = GaussianMixture(n_components=2)
-    test=pd.read_csv("../../bibliothq.csv")
-    test.index=test["livres"]
-    del test["livres"]
-    gmm.fit(test)
-    print(gmm.means_)
-    print(gmm.covariances_)
-"""
+pafgmm = PafGMM(data, 10)
+categorisation = pafgmm.result()
+
+contraste = Contraste(categorisation, pafgmm.model.means_, 10).calcul()
+
+print(categorisation)
+print(contraste)
+
+
 

d60e0fa5b98eb1004082bbde88cc2bec9d9a6278
Antoine Bellami <antoine@localhost.localdomain>
2018-06-19 10:15:02 +0200

    nettoyage de contrastes.py

--- src/processing/contrastes.py
+++ src/processing/contrastes.py
@@ -1,27 +1,23 @@
 import cluster
 
-import sys
 import pandas as pd
 import numpy as np
-import sklearn.metrics as sm
-import matplotlib.pyplot as plt
-import math as m
 
 def calcDiffs(cluster):
     """
-    This function computes the differences between the points of the cluster and its center
-    Then, it standardizes these by the convariance matrix, and keeps only the components that
-    are greater than the max divided by 10 (arbitrary)
+        This function computes the differences between the points of the cluster and its center
+        Then, it standardizes these by the covariance matrix, and keeps only the components that
+        are greater than the max divided by 10 (arbitrary)
     
-    returns a DataFrame containing the calculated rows
+        returns a DataFrame containing the calculated rows
     """
     diffs = pd.DataFrame(columns = cluster.points.columns)
     for idx, row in cluster.points.iterrows():
         new = row - cluster.centre # computing the difference
         norm_new = pd.Series(np.dot(np.linalg.inv(cluster.matriceCov), new), index = new.index) # standardization
-        maxi = norm_new.max()
-        m = norm_new < maxi/10 
-        norm_new.mask(m, other = 0, inplace = True) # selection of the greatest values (greater than max/10)
+        maximum = norm_new.max()
+        condition = norm_new < maximum/10
+        norm_new.mask(condition, other = 0, inplace = True) # selection of the greatest values (greater than max/10)
         diffs = diffs.append(norm_new, ignore_index = True)
     return diffs
 

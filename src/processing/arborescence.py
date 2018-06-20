6bf64538a3c34de552068cf0e7e9fd76f44d6308
Louis de Monterno <louis.penetdemonterno@gmail.com>
2018-06-20 09:55:17 +0200

    ajout d'une méthode d'actualisation de la matrice de covariance

--- src/processing/arborescence.py
+++ src/processing/arborescence.py
@@ -11,6 +11,8 @@ import math
 import numpy as np
 import pandas as pd
 
+_CONSTANTE = 0.001
+
 def argmax_(liste,key=lambda x:x):
 	if len(liste) == 0:
 		assert "liste vide"
@@ -28,14 +30,24 @@ def argmax_(liste,key=lambda x:x):
 			idx = i
 			maximum = key(test)
 
-def distance(noeud,point):
-	if type(noeud) == tuple:
-		
+class Feuille:
+	
+	def __init__(self, titre, point):
+		self.titre = titre
+		self.point = point
+	
+	def feuille(self):
+		return True
+	
+	def __iter__(self):
+		return iter(self.point)
+	
+	def matrice(self):
+		return np.eye(len(point)) * _CONSTANTE
 
 class Arbre(Cluster):
-	
-	def __init__(self, points):
-		
+			
+	self.nombre_instance = 0	
 	
 	def mahalanobis(self, point):
 		point_centre = np.array([point]) - np.array([centre])
@@ -45,20 +57,40 @@ class Arbre(Cluster):
 	
 	def __init__(self, enfants, pater=None, label=None):
 		self.enfants = enfants
+		descendants = self._private_liste_points()
+		self.nombre_descendant = len(descendants)
+		
 		self.label = label
 		self.pater = pater
-		self.groupe = Cluster(enfants, None, label)
+		self.groupe = Cluster.__init__(self, descendants, None, self.nombre_instance)
+		self.nombre_instance += 1
 		
-		
+	def feuille():
+		return False
+	
 	def nouvelle_donne(self, livre):
 		min_enfant, min_distance = argmax_(enfants, key=lambda x:-)
 	
-	def actualise_matrice(self):
-		liste_points = self._private_liste_points()
-		self.centre = liste_points.mean()
-		self.matriceCov = self.matriceCovariance(liste_points)
-		random = np.random.random(len(enfants[0]),len(enfants[0]))
-		self.matriceCov += pd.DataFrame(random * 10e-6)
+	def actualise_matrice(self, point):
+		
+		"""actualise l'espérence et la matrice de covariance
+		d'un cluster après l'ajout d'un point, en temps constant par 
+		rapport au nombre de points."""
+		
+		self.matriceCov /= 1 + 1/self.nombre_descendant
+		for i in range(len(point)):
+			for j in range(i, len(point)):
+				
+				self.matriceCov += ( self.nombre_descendant * point[i] * point[j] - \
+									self.centre[i] * point[j] - \
+									self.centre[j] * point[i] ) / \
+									(self.nombre_descendant + 1)**2
+		self.centre = (self.nombre_descendant * self.centre + point)/(self.nombre_descendant + 1)
+		
+		self.nombre_descendant += 1
+		
+		#random = np.random.random(len(enfants[0]),len(enfants[0]))
+		#self.matriceCov += pd.DataFrame(random * 10e-6)
 	
 	def _private_liste_points(self):
 		points = []

74a993676309a848e1d60f4bac4b33a8e146387e
Antoine Bellami <antoine@localhost.localdomain>
2018-06-21 13:59:41 +0200

    nouvelle version de contrasteur.py fonctionnelle

--- rebirth/src/contrasteur.py
+++ rebirth/src/contrasteur.py
@@ -1,48 +1,55 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Wed Jun 20 13:57:11 2018
+Created on Thu Jun 21 10:32:13 2018
 
 @author: antoine
 
 On prend en entrée une liste d'objets cluster (comportant un dataframe, un vecteur centre,
-un label et une liste de sous-clusters)
-On prend également un nouvel élément en entrée (un array)
+un label) catégorisation ; et une liste d'objets clusters contraste
+On prend également un nouvel élément en entrée (un array numpy)
 
-On ressort un array contenant la catégorie et la dimension dominante de l'élément
+On ressort un array contenant la catégorie et une liste d'adjectifs correspondant à l'élément
 """
 
 import numpy as np
 import pandas as pd
 from math import sqrt
 
-def result(listeClusters, element ) :
+def result(clustersCategories, clustersContrast, element ) :
     """ on attribue à element le cluster dont il est le plus proche du centre """
     distance_min = -1
-    for cluster in listeClusters :
+    for cluster in clustersCategories :
         distance_tmp = distanceEuclidienne(cluster.centre, element)
         if (distance_tmp < distance_min or distance_min < 0) :
             distance_min = distance_tmp
-            cluster_element = cluster
+            cluster_category = cluster
     
-    label = cluster_element.getLabel()
-    dataframe_cluster = cluster_element.getDataFrame()
-    center_cluster = cluster_element.getCenter()
+    label = cluster_category.getLabel()
     
-    data = element - center_cluster
+    """ on attribue à element la liste de labels associé au cluster de contrastes
+    dans lequel il se trouve s'il en est assez proche en nombre d'écarts-types """
+
+    adjectifs = "pas d'adjectif pertinent trouvé"
+    distance_min = -1
+    for cluster in clustersContrast :
+        distance_tmp = distanceEuclidienne(cluster.centre, element)
+        """ si une des composantes de la différence entre element et le centre du cluster
+            dépasse 3 variances, alors la condition n'est pas remplie """
+        if (distance_tmp < distance_min or (distance_min < 0 and assezProche(element, cluster))) :
+            distance_min = distance_tmp
+            cluster_contrast = cluster
     
-    values = []
+    adjectifs = cluster_contrast.getLabel()
+    
+    return label, adjectifs
+
+def assezProche(point, cluster):
+    data = point - cluster.getCenter()
     for k in range(len(element)):
-        values.append(abs(data[k])/sqrt(np.array(dataframe_cluster.var(axis=0))[k]))
-    
-    indiceDominant = values.index(max(values))
-    dimensionDominante = dataframe_cluster.columns[indiceDominant]    
-            
-    """ on attribue à element la dimension dominante sur laquelle la difference
-    entre element et le centre du cluster auquel il appartient a la valeur la plus
-    importante par rapport à la variance sur cette dimension """
-    
-    return label, dimensionDominante
+        if (abs(data[k])/sqrt(np.array(cluster.getDataFrame().var(axis=0))[k]) > 3):
+            return False
+    return True
 
 def distanceEuclidienne(point_1, point_2):
     distance = 0
@@ -54,14 +61,9 @@ if __name__ == "__main__":
     ar = np.array([[1.1, 2, 3.3, 4], [2.7, 10, 5.4, 7], [5.3, 9, 1.5, 15]])
     df = pd.DataFrame(ar, index = ['a1', 'a2', 'a3'], columns = ['A', 'B', 'C', 'D'])
 
-    element = np.array([0, 5, 3.5, 8])
+    element = np.array([3, 5, 3.5, 8])
     center = np.array([3.03, 7, 3.4, 8.67])
 
-    values = []
     for k in range(len(element)):
-        values.append((abs(element[k] - center[k]))/sqrt(np.array(df.var(axis=0))[k]))
-
-    indiceDominant = values.index(max(values))
-    dimensionDominante = df.columns[indiceDominant]
-
-    print(dimensionDominante)
+        if ((abs(element[k] - center[k]))/sqrt(np.array(df.var(axis=0))[k]) > 3):
+            print((abs(element[k] - center[k]))/sqrt(np.array(df.var(axis=0))[k]))

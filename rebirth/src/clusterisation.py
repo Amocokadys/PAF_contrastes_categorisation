88502efff56b885c6c99acbda2f2fa7773101caa
Aurélien Blicq <blicqaurelien@gmail.com>
2018-06-23 19:15:27 +0200

    changement de la labelisation des clusters de contraste

--- rebirth/src/clusterisation.py
+++ rebirth/src/clusterisation.py
@@ -20,8 +20,13 @@ class Clusterisation:
         # get clusters from data
         clusters=[]
         for i in range(nb_clusters):
-            clusters.append(Cluster(dataframe.loc[dataframe['category']==i],\
-                                    means[i], self.isContrast))# for each cluster, selects the points in the cluster
+            if not self.isContrast:
+                clusters.append(Cluster(dataframe.loc[dataframe['category']==i],\
+                                        means[i]))# for each cluster, selects the points in the cluster
+
+            else:
+                clusters.append(ContrastCluster(dataframe.loc[dataframe['category']==i],\
+                                        means[i]))# for each cluster, selects the points in the cluster
     
         return clusters
 
@@ -41,7 +46,7 @@ class Cluster:
     def __repr__(self):
         return (" dataframe: \n"+str(self.points)+"\n center:"+str(self.centre)+"\n label:"+str(self.label))
 
-    def __init__(self,points,centre, isContrast):
+    def __init__(self,points,centre):
         """ if (len(points) == 2) :
             pointMoyen = [(points[i][0] + points[i][1] + 1.0001)/2 for i in points.columns]
             new_data = pd.DataFrame([pointMoyen], columns = points.columns, index = pd.RangeIndex(start=2, stop=3, step=1))
@@ -49,9 +54,8 @@ class Cluster:
         """
         self.centre=centre
         self.points=points #un dataframe
-        self.propDict={}
         self.label=[]
-        self.updateLabel(isContrast)
+        self.updateLabel()
         self.subClusters=None
         self.sharpedData = None
         
@@ -82,48 +86,58 @@ class Cluster:
         self.points = self.points.append(new_data)
 
 
-    def updatePropDict(self, isContrast):
+    def updatePropDict(self):
         """
         this method computes a dictionnary that contains the proportions of presence of 
         each label among the data of the cluster
         """
         self.propDict = {}
         for idx in self.points.index:
-            if not isContrast:
-                lst_idx = [idx.split('#')[0]]
-            else:
-                lst_idx = idx.split('#')[1].split('~')[:-1]
-            for i in lst_idx:
-                self.propDict[i] = 0
+            i = idx.split('#')[0]
+            self.propDict[i] = 0
 
         for idx, row in  self.points.iterrows():
-            if not isContrast:
-                lst_idx = [idx.split('#')[0]]
-            else:
-                lst_idx = idx.split('#')[1].split('~')[:-1]
-            for i in lst_idx:
-                self.propDict[i] += 1
+            i = idx.split('#')[0]
+            self.propDict[i] += 1
 
         for key in self.propDict.keys():
             self.propDict[key] /= len(self.points)
-        
-        if isContrast:
-            print(self.propDict)
 
-
-    def updateLabel(self, isContrast):
-        """
-        this method gets the major labels of the cluster by selecting only the ones that
-        at least 50% of the cluster have
-        """
-        self.updatePropDict(isContrast)
+    def updateLabel(self):
+        self.updatePropDict()
         for k in self.propDict.keys():
-            self.label.append(k)
-        if isContrast:
-            print(self.label)
-        
-        
-        
+            if self.propDict[k] > 0.5:
+                self.label.append(k)
+
+
+class ContrastCluster(Cluster):
+
+    def __init__(self,points,centre):
+        """ if (len(points) == 2) :
+            pointMoyen = [(points[i][0] + points[i][1] + 1.0001)/2 for i in points.columns]
+            new_data = pd.DataFrame([pointMoyen], columns = points.columns, index = pd.RangeIndex(start=2, stop=3, step=1))
+            points = points.append(new_data)
+        """
+        self.centre=centre
+        self.points=points #un dataframe
+        self.label=[]
+        self.updateLabel()
+        self.subClusters=None
+        self.sharpedData = None
+
+    def updateLabel(self):
+       """
+       this method gives labels to the cluster according to the values of
+       it center (if greater than 1, gives the label)
+       """
+       for k in range(len(self.centre)):
+           if self.centre[k] >= 1:
+               self.label.append(self.points.columns[k])
+               print(self.label[-1], self.centre[k])
+
+       if self.label == []:
+           self.label.append("normal")
+
         
 class SubCluster(Cluster):
     """To use the subclusters, herites from Cluster"""

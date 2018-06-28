85392deb0ddbeb941d5c5bb8dcf655efd645b432
Votre Nom <lpenet@telecom-paristech.fr>
2018-06-28 11:20:58 +0200

    je sais pas ce que j'ai modifié

--- src/processing/arborescence.py
+++ src/processing/arborescence.py
@@ -226,4 +226,35 @@ class Arbre(Ensemble):
 
 	
 
-
+if __name__ == "__main__":
+	racine = Arbre([], "~")
+	feuilles = []
+	predistribution = []
+	
+	with open("../../jeux de donne/animaux.csv", "r") as fichier:
+		cursor = csv.reader(fichier, delimiter=",")
+		
+		for ligne in cursor:
+			if len(predistribution) == 0:
+				predistribution = ligne[1:]
+			elif len(Ensemble.distribution) == 0:
+				for i in range(len(predistribution)):
+					if ligne[i+1] == "none":
+						Ensemble.distribution[predistribution[i]] = None
+					else:
+						print("-",ligne[i],"-")
+						Ensemble.distribution[predistribution[i]] = float(ligne[i+1])
+			else:
+				dico = {}
+				for i in range(1, len(ligne)):
+					if len(ligne[i]) > 0:
+						dico[predistribution[i-1]] = float(ligne[i])
+				feuilles.append(Feuille(dico, ligne[0]))
+				
+	
+	while len(feuilles) > 0:
+		au_sort = randint(0, len(feuilles)-1)
+		racine += feuilles[au_sort]
+		del feuilles[au_sort]
+	
+	racine.dessin()

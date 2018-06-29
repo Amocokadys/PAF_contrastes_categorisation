4b10ada2590638c3bca4a7d649e76f0e418a3b7a
Clément Bonet <clementbonet@hotmail.fr>
2018-06-29 16:53:09 +0200

    Up cartouches

--- version_incrementale/processing/interfaceGraph.py
+++ version_incrementale/processing/interfaceGraph.py
@@ -1,3 +1,13 @@
+"""
+Organisme de référence …… : Télécom ParisTech (https://www.telecom-paristech.fr/)
+Contexte du projet ……………… : Projet PAF (https://paf.telecom-paristech.fr/)
+Sujet ………………………………………………… : Contraste et catégorisation (http://teaching.dessalles.fr/Projects/P18051801.html)
+Auteurs …………………………………………… : Bastien Vagne, Louis Penet de Monterno, Benoît Malézieux,Clément Bonet, Aurélien Blicq, Antoine Bellami
+Date …………………………………………………… : 25/06/2018
+Description du fichier …… : Cartouche
+"""
+
+
 from tkinter import *
 from PIL import Image, ImageTk
 import numpy as np
